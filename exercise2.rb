0405239acbee51fb2cf5026ce902fa37c5f3ed75
Miranda Gagnon <miranda.kay.gagnon@gmail.com>
2016-01-12 14:09:55 -0500

    complete excerise 2.

---
+++ exercise2.rb
@@ -0,0 +1,20 @@
+# I would create a variable to hold the price of the meal and multiply that by
+# 20% and use puts to show the use
+
+tip_amount = 67 * 0.2
+puts "A good tip would be $#{tip_amount}."
+
+#use to_s to convert integer to string
+
+puts "This is how to convert an integer to a string " + 4.to_s
+
+#Use #{} to do the calculation within the string then output
+
+result = "The result is #{45628 * 7839}"
+puts result
+
+#the result is true
+
+my_result = (10 < 20 && 30 < 20) || !(10 == 11)
+
+puts my_result 
